6e7c19649e6ffc854bdec358486c07d899bed14e
Bartlomiej Kozaryna <kozarynabartlomiej@gmail.com>
2022-11-30 12:44:20 +0100

    Language decision

--- nextDay.py
+++ nextDay.py
@@ -1,11 +1,12 @@
 import os
+import subprocess
 
-EXT = ".py"
+EXT = ".scala"
 YEAR = "2022"
 URL = "https://adventofcode.com"
 
 def is_day(name: str) -> bool:
-    return name.startswith("Day-")
+    return name.startswith("day-")
 
 def get_day_number(name: str) -> int:
     try:
@@ -18,11 +19,18 @@ for file in os.listdir():
     if os.path.isdir(file) and is_day(file):
        day = max(day, get_day_number(file) + 1)
 
-dirname = f"Day-{day}"
+dirname = f"day-{day}"
 
-os.mkdir(dirname)
-open(dirname + "/first" + EXT, "x")
-open(dirname + "/second" + EXT, "x")
+subprocess.run(["sbt", "new", "scala/scala3.g8"], input=dirname.encode(), capture_output=True)
+
+p = "/src/main/scala"
+
+open(dirname + p + "/First" + EXT, "x")
+open(dirname + p + "/Second" + EXT, "x")
+os.remove(dirname + "/README.md")
 with open(dirname + "/README.md", "x") as f:
     f.write(f"# Day-{day}  \n")
     f.write(f"## [Link]({URL}/{YEAR}/day/{day})  \n")
+
+open(dirname + "/input-1.txt", "x")
+open(dirname + "/input-2.txt", "x")
